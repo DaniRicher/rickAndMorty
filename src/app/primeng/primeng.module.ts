a93a38d7057da4c98827eeae6b904835825c6c6e
Daniel Richerson <richerdani3@gmail.com>
2022-09-10 15:29:05 -0500

    Base inicial de la aplicación

---
+++ src/app/primeng/primeng.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+//PrimeNg
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  exports: [
+    ButtonModule
+  ]
+})
+export class PrimengModule { }
